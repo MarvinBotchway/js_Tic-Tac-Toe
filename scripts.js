c74fa14d6b85e0ad2e6f8980e93de3489e15c69c
Marvin Botchway <marvinbotchway@outlook.com>
2023-12-12 18:15:54 +0000

    Add GameBoard
    Used the factory function Module pattern to create an instance
    of a gameboard UI.

--- scripts.js
+++ scripts.js
@@ -1,1 +1,26 @@
-console.log("Working!");
+const createGameboard = (function () {
+    let gameBoard = [];
+    const boardContainer = document.querySelector("#board-container");
+    const board = document.createElement("div");
+    board.classList += "board";
+   
+    function createUI () {
+        for (let i = 0; i < 3; i++) {
+            for (let j = 0; j < 3; j++) {
+                const cell = document.createElement("div");
+                cell.dataset.position = `${i},${j}`;
+                cell.classList += "cell"
+                board.appendChild(cell);
+
+                gameBoard.push([i, j]);
+            }
+        }
+        boardContainer.appendChild(board);
+    } 
+   
+    
+    return {createUI}
+})();
+
+
+createGameboard.createUI();
