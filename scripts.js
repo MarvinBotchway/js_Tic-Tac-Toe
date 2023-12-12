eb3430ebecd19d46f95cdb0afa56b9655ddf499c
Marvin Botchway <marvinbotchway@outlook.com>
2023-12-12 23:40:55 +0000

    Refactored Code To Use A Display Controller
    Added an updateDisplay function which updates the Display
    and a displayController.

--- scripts.js
+++ scripts.js
@@ -1,26 +1,55 @@
 const createGameboard = (function () {
     let gameBoard = [];
-    const boardContainer = document.querySelector("#board-container");
-    const board = document.createElement("div");
-    board.classList += "board";
    
-    function createUI () {
+    function createBoard () {
         for (let i = 0; i < 3; i++) {
             for (let j = 0; j < 3; j++) {
-                const cell = document.createElement("div");
-                cell.dataset.position = `${i},${j}`;
-                cell.classList += "cell"
-                board.appendChild(cell);
+                const cellDetails = {
+                    coordinateX : i,
+                    coordinateY : j,
+                    played : "X"
+                };
 
-                gameBoard.push([i, j]);
+                gameBoard.push(cellDetails);
             }
+        }
+    }
+
+    function getBoard () {
+        createBoard();
+        return gameBoard
+    };
+   
+    
+    return {getBoard}
+})();
+
+const displayController = (function () {
+    const gameBoard = createGameboard.getBoard();
+    
+    return {gameBoard};
+})();
+
+const updateDisplay = (function () {
+    const gameBoard = displayController.gameBoard;
+    
+    function createBoard() {
+        const boardContainer = document.querySelector("#board-container");
+        const board = document.createElement("div");
+        board.classList += "board";
+ 
+        for (let i = 0; i < 9; i++) {
+            const cell = document.createElement("div");
+            cell.dataset.position = `${gameBoard[i].coordinateX},${gameBoard[i].coordinateY}`;
+            cell.dataset.played = "";
+            cell.classList += "cell"
+            board.appendChild(cell);
         }
         boardContainer.appendChild(board);
     } 
-   
-    
-    return {createUI}
+
+    return {createBoard};
 })();
 
+updateDisplay.createBoard();
 
-createGameboard.createUI();
